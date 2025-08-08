4e0c05a0f67cd02139ded131478c7cc9c734d658
DragonCanFlies <lauratabay@hotmail.fr>
2025-08-08 23:41:26 +0200

    go back to one header

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: latabagl <latabagl@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 13:49:33 by latabagl          #+#    #+#             */
-/*   Updated: 2025/08/04 13:53:03 by latabagl         ###   ########.fr       */
+/*   Updated: 2025/08/08 23:40:57 by latabagl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,6 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <limits.h>
-# include "operations.h"
-# include "algo.h"
-# include "utils.h"
 
 #define TRUE 1
 #define FALSE 0
@@ -46,5 +43,27 @@ int     ft_strcmp(const char *s1, const char *s2);
 int	    is_valid_argument(char *arg);
 long	my_atoi(char *arg);
 int     check_and_convert(char *arg, int *is_input_valid);
+
+void	sort_2(t_list **stack_a, t_list **stack_b);
+void	selection_sort(t_list **stack_a, t_list **stack_b);
+
+void	pa(t_list *stack_a, t_list *stack_b);
+void	pb(t_list *stack_a, t_list *stack_b);
+void	push(t_list *stack_1, t_list *stack_2);
+
+void    ra(t_list **stack_a);
+void    rb(t_list **stack_b);
+void    rr(t_list **stack_a, t_list **stack_b);
+void    rotate(t_list **stack);
+
+void	rra(t_list **stack_a);
+void	rrb(t_list **stack_b);
+void	rrr(t_list **stack_a, t_list **stack_b);
+void	reverse_rotate(t_list **stack);
+
+void	sa(t_list *stack_a);
+void	sb(t_list *stack_b);
+void	ss(t_list *stack_a, t_list *stack_b);
+void	swap(t_list *stack);
 
 #endif
